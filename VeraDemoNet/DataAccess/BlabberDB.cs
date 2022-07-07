d896e1d322ab14f0edd5728c02e020bd229579a6
Vladimir Huzaryk <vhuzaryk@lms365.com>
2022-07-07 13:29:41 +0300

    8-Move-connection-string-to-web.config

--- VeraDemoNet/DataAccess/BlabberDB.cs
+++ VeraDemoNet/DataAccess/BlabberDB.cs
@@ -1,25 +1,21 @@
-﻿using System.Data.Entity;
+﻿using System.Configuration;
+using System.Data.Entity;
 
 namespace VeraDemoNet.DataAccess
 {
-    public class BlabberDB : DbContext  
+    public class BlabberDB : DbContext
     {
-        const string DATA_SOURCE = "(localdb)\\MSSQLLocalDB";
-        const string DB_FILENAME = "|DataDirectory|VeraDemoNet.mdf";
-        const string LOGIN = "admin";
-        const string PASSWORD = "admin123";
-        const string OPTIONS = "Integrated Security=True;User Instance=False";
-        public BlabberDB() : base($"Data Source={DATA_SOURCE};AttachDbFilename={DB_FILENAME};User Id={LOGIN};Password={PASSWORD};{OPTIONS}")  
-        {  
-        }  
-  
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)  
-        {  
-            modelBuilder.Entity<User>().HasKey(x=>x.UserName);
+        public BlabberDB() : base(ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(x => x.UserName);
         }
 
 
-  
-        public DbSet<User> Users { get; set; }  
-    }  
+
+        public DbSet<User> Users { get; set; }
+    }
 }
